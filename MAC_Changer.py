bc59a754da8828f3a6da403a094207025055c981
nerdynerd09 <63356733+nerdynerd09@users.noreply.github.com>
2020-06-09 13:01:05 +0530

    Update MAC_Changer.py

--- MAC_Changer.py
+++ MAC_Changer.py
@@ -23,12 +23,13 @@ def mac(interface , new_mac):
   
   subprocess.call(["ifconfig", interface , "down"])
   subprocess.call(["ifconfig", interface , "hw", "ether", new_mac])
+  subprocess.call(["ifconfig", interface , "up"])
   print("Your MAC Adress has been changed to", new_mac)
 try:
   while True:
     intro()
-    interface = raw_input(green + "[+] Enter an interface >>" + reset)
-    new_mac = raw_input (green + "[+] Enter a MAC Address >>" + reset)
+    interface = raw_input(green + "[+] Enter an interface >> " + reset)
+    new_mac = raw_input (green + "[+] Enter a MAC Address >> " + reset)
 
     mac(interface , new_mac)
 except KeyboardInterrupt:
